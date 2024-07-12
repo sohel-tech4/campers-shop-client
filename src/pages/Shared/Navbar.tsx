8011c905bc361f18a49ca4aaeb878af9321727ca
Sohel Rana <soheltech4@gmail.com>
2024-07-13 02:43:31 +0600

    add navbar

--- src/pages/Shared/Navbar.tsx
+++ src/pages/Shared/Navbar.tsx
@@ -1,7 +1,45 @@
-const Navbar = () => {
+import { Button } from "@/components/ui/button";
+import { Link, useLocation } from "react-router-dom";
+
+function Navbar() {
+  const location = useLocation();
+  const Nav = (
+    <>
+      <Link
+        to="/"
+        className={`px-3 hover:text-black rounded hover:duration-1000 ${
+          location.pathname === "/"
+            ? "active-link font-bold bg-opacity-80 text-black"
+            : ""
+        }`}
+      >
+        Home
+      </Link>
+      <Link
+        to="/aboutus"
+        className={`px-3  hover:text-black rounded hover:duration-1000 ${
+          location.pathname === "/aboutus"
+            ? "active-link font-bold bg-opacity-80 text-black"
+            : ""
+        }`}
+      >
+        About Us
+      </Link>
+      <Link
+        to="/contact"
+        className={`px-3  hover:text-black rounded hover:duration-1000 ${
+          location.pathname === "/contact"
+            ? "active-link font-bold bg-opacity-80 text-black"
+            : ""
+        }`}
+      >
+        Contact
+      </Link>
+    </>
+  );
   return (
     <>
-      <div className="navbar bg-base-100">
+      <div className="navbar bg-base-100 shadow-md">
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -24,56 +62,25 @@ const Navbar = () => {
               tabIndex={0}
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
             >
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a>Item 3</a>
-              </li>
+              {Nav}
             </ul>
           </div>
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+          <div className="md:flex justify-center items-center gap-3">
+            <img
+              src="../../../public/the Camp Logo.png"
+              className="w-16"
+              alt=""
+            />
+            <h1 className="font-bold">The Campsite Corner</h1>
+          </div>
         </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
-          </ul>
-        </div>
+        <div className="navbar-center hidden lg:flex">{Nav}</div>
         <div className="navbar-end">
-          <a className="btn">Button</a>
+          <Button>Login</Button>
         </div>
       </div>
     </>
   );
-};
+}
 
 export default Navbar;
