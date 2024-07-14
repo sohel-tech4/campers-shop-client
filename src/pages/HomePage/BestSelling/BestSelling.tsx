65858231c0d18d430dc547132ceca0f9417039ce
Sohel Rana <sohelranagtc@gmail.com>
2024-07-14 13:52:22 +0600

    add type of card

--- src/pages/HomePage/BestSelling/BestSelling.tsx
+++ src/pages/HomePage/BestSelling/BestSelling.tsx
@@ -1,13 +1,16 @@
 import products from "../../../../public/product.json";
 import Card from "./Card";
+import { TProduct } from "./card.type";
+
 function BestSelling() {
+
   return (
     <div>
       <div>
         <h1>Best Selling</h1>
         <p>Chose the best</p>
       </div>
-      {products.map((item): any => {
+      {products.map((item): TProduct[] => {
         <Card key={item?.productName} item={item}></Card>;
       })}
     </div>
---
+++ src/pages/HomePage/BestSelling/card.type.ts
@@ -0,0 +1,9 @@
+export type TProduct = {
+    productName: string;
+    price: number;
+    stockQuantity: number;
+    description: string;
+    category: string;
+    ratings: number;
+    images: string[];
+};
